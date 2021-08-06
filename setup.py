42ca5ac5f86f654b590596fc651455987a49c7d1
Maarten Nieber <hallomaarten@yahoo.com>
2021-08-06 20:55:11 +0200

    Fix setup.py and bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
+
 setup(
   name='django-litecrop',
   packages=['django_litecrop'],
-  version='0.2.0',
+  version='0.2.1',
   description='Non-magical Jcrop based image cropping for Django',
   author='Maarten Nieber',
   author_email='hallomaarten@yahoo.com',
-  url='https://github.com/mnieber/django_litecrop',
-  download_url='https://github.com/mnieber/django-litecrop/tarball/0.2.0',
+  url='https://github.com/mnieber/django-litecrop',
+  download_url='https://github.com/mnieber/django-litecrop/tarball/0.2.1',
   keywords=['crop', 'cropping', 'django', 'jcrop'],
   classifiers=[],
 )
